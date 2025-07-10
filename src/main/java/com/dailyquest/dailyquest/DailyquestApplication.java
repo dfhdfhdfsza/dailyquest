f55766ea9c5f8a80663e5cb666dd7f68198233c2
유호재 <dbghwidkdh@gmail.com>
2025-07-10 20:35:49 +0900

    프로젝트 생성

---
+++ src/main/java/com/dailyquest/dailyquest/DailyquestApplication.java
@@ -0,0 +1,13 @@
+package com.dailyquest.dailyquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyquestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dailyquest/dailyquest/DailyquestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailyquest.dailyquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyquestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
