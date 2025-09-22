6930739841af96d24a532e8d7fe55a56f83d6e6c
dfhdfhdfsza <dbghwidkdh@gmail.com>
2025-09-22 22:18:48 +0900

    ci 오류수정

--- src/test/java/com/dailyquest/dailyquest/TestConfig.java
+++ src/test/java/com/dailyquest/dailyquest/TestConfig.java
@@ -5,6 +5,8 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 
 import static org.mockito.Mockito.mock;
 
@@ -25,5 +27,11 @@ public class TestConfig {
     @Bean
     public JavaMailSender javaMailSender() {
         return mock(JavaMailSender.class);
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        // 빈 레지스트리여도 SecurityFilterChain 빌드는 통과합니다.
+        return new InMemoryClientRegistrationRepository();
     }
 }
