9405639e7ab36dc8d0965d2cbfe0471187a660f6
dfhdfhdfsza <dbghwidkdh@gmail.com>
2025-09-22 20:38:12 +0900

    ci 오류수정

--- src/test/java/com/dailyquest/dailyquest/TestConfig.java
+++ src/test/java/com/dailyquest/dailyquest/TestConfig.java
@@ -5,6 +5,7 @@ import org.mockito.Mockito;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import static org.mockito.Mockito.mock;
 
@@ -12,12 +13,13 @@ import static org.mockito.Mockito.mock;
 public class TestConfig {
 
     @Bean
-    RedisTemplate<?,?> redisTemplate() {
-        return mock(RedisTemplate.class);
-    }
-    @Bean
     public LoginEndpointRateLimitFilter loginEndpointRateLimitFilter() {
         // 의존성 많아도 상관없이 Mock으로 대체
         return mock(LoginEndpointRateLimitFilter.class);
+    }
+
+    @Bean
+    public StringRedisTemplate stringRedisTemplate() {
+        return mock(StringRedisTemplate.class);
     }
 }
