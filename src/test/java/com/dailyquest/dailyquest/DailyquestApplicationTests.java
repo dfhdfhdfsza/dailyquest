e7ba043b288495cad3ac47738474906d6e87e579
dfhdfhdfsza <dbghwidkdh@gmail.com>
2025-09-22 17:02:05 +0900

    ci 설정

--- src/test/java/com/dailyquest/dailyquest/DailyquestApplicationTests.java
+++ src/test/java/com/dailyquest/dailyquest/DailyquestApplicationTests.java
@@ -2,8 +2,11 @@ package com.dailyquest.dailyquest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DailyquestApplicationTests {
 
 	@Test
